9c2b5479289894e94475c02212789fb1cc029dac
Marco André Mendes <marcoandre@gmail.com>
2026-03-10 14:49:45 -0300

    feat: criação da API para Livro

--- core/admin.py
+++ core/admin.py
@@ -54,4 +54,5 @@ class UserAdmin(BaseUserAdmin):
 admin.site.register(models.Autor)
 admin.site.register(models.Categoria)
 admin.site.register(models.Editora)
+admin.site.register(models.Livro)
 admin.site.register(models.User, UserAdmin)
